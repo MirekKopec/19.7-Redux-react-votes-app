9c18141527971c170a7e00231feca82acd3858d4
MirekKopec <mkopec95@gmail.com>
2018-02-25 12:41:25 +0100

    added DevTools

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import CommentsList from './CommentsListContainer';
+import DevTools from './DevTools';
 
 const App = () => {
   return (
     <div className="App">
       Tu będzie lista komentarzy!
       <CommentsList/>
+      <DevTools />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import reducer from './reducer.js';
 import { addComment } from './actions.js';
+// import { createLogger } from 'redux-logger';
+import DevTools from './DevTools';
 
-const store = createStore(reducer);
+
+//const logger = createLogger();
+
+const store = createStore(
+  reducer,
+  //applyMiddleware(logger)
+  DevTools.instrument()
+);
+
 
 ReactDOM.render(
     <Provider store={store}>
